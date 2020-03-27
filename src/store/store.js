7c5e5ae65d5a57c2a7dd9cfdd9f0d04b4ca42289
Prakash <pk4501020@gmail.com>
2020-03-27 16:16:59 +0530

    Updated store.js for deployment

--- src/store/store.js
+++ src/store/store.js
@@ -18,7 +18,7 @@ export const store=new Vuex.Store({
     actions:{
         login:(context,data)=>{
             return new Promise((resolve,reject)=>{
-                    axios.post('http://127.0.0.1:8000/api/login/',data).then((response)=>{
+                    axios.post('http://prakash3720.pythonanywhere.com/api/login/',data).then((response)=>{
                     let res=response.data
                     let token="Token "
                     token=token+res.token
@@ -34,7 +34,7 @@ export const store=new Vuex.Store({
         },
         register:(context,data)=>{
             return new Promise((resolve,reject)=>{
-                axios.post('http://127.0.0.1:8000/api/profile/',data).then((response)=>{
+                axios.post('http://prakash3720.pythonanywhere.com/api/profile/',data).then((response)=>{
                     resolve({status:1})
                 })
                 .catch(err=>{
@@ -44,9 +44,9 @@ export const store=new Vuex.Store({
         },
         home:(context)=>{
             return new Promise((resolve,reject)=>{
-                axios.get('http://127.0.0.1:8000/api/profile/').then(dat=>{
+                axios.get('http://prakash3720.pythonanywhere.com/api/profile/').then(dat=>{
                     context.state.user=dat.data[0]
-                    axios.get('http://127.0.0.1:8000/api/todo/').then(todos=>{
+                    axios.get('http://prakash3720.pythonanywhere.com/api/todo/').then(todos=>{
                         context.state.user.todos=todos.data
                         resolve(context.state.user)
                     })
@@ -61,8 +61,8 @@ export const store=new Vuex.Store({
         },
         add:(context,data)=>{
             return new Promise((resolve,reject)=>{
-                axios.post('http://127.0.0.1:8000/api/todo/',data).then(dat=>{
-                    axios.get('http://127.0.0.1:8000/api/todo/').then(todos=>{
+                axios.post('http://prakash3720.pythonanywhere.com/api/todo/',data).then(dat=>{
+                    axios.get('http://prakash3720.pythonanywhere.com/api/todo/').then(todos=>{
                         resolve(todos.data)
                     })
                     .catch(err=>{
@@ -76,8 +76,8 @@ export const store=new Vuex.Store({
         },
         delete:(context,data)=>{
             return new Promise((resolve,reject)=>{
-                axios.delete(`http://127.0.0.1:8000/api/todo/${data.index}/`).then(()=>{
-                    axios.get('http://127.0.0.1:8000/api/todo/').then(todos=>{
+                axios.delete(`http://prakash3720.pythonanywhere.com/api/todo/${data.index}/`).then(()=>{
+                    axios.get('http://prakash3720.pythonanywhere.com/api/todo/').then(todos=>{
                         resolve(todos.data)
                     })
                     .catch(err=>{
@@ -91,8 +91,8 @@ export const store=new Vuex.Store({
         },
         edit:(context,data)=>{
             return new Promise((resolve,reject)=>{
-                axios.patch(`http://127.0.0.1:8000/api/todo/${data.index}/`,{todo:data.todo}).then(()=>{
-                    axios.get('http://127.0.0.1:8000/api/todo/').then(todos=>{
+                axios.patch(`http://prakash3720.pythonanywhere.com/api/todo/${data.index}/`,{todo:data.todo}).then(()=>{
+                    axios.get('http://prakash3720.pythonanywhere.com/api/todo/').then(todos=>{
                         resolve(todos.data)
                     })
                     .catch(err=>{
